c5b5129d89ea05861200238af0dd2cbcfcd341a5
Lucky <luckytomar1801@gmail.com>
2026-02-02 17:54:52 +0530

    done

---
+++ spring-core-project/src/main/java/com/rays/test/TestUser.java
@@ -0,0 +1,48 @@
+package com.rays.test;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+
+public class TestUser {
+
+	public static void main(String[] args) {
+
+		BeanFactory factory = new XmlBeanFactory(new ClassPathResource("user.xml"));
+
+		User dto = (User) factory.getBean("user");
+
+		System.out.println(dto.getName());
+		System.out.println(dto.getAddress());
+
+		System.out.println("------------------");
+
+		dto = (User) factory.getBean("user1");
+
+		System.out.println(dto.getName());
+		System.out.println(dto.getAddress());
+
+		System.out.println("------------------");
+
+		dto = (User) factory.getBean("user2");
+
+		System.out.println(dto.getName());
+		System.out.println(dto.getAddress());
+
+		System.out.println("------------------");
+
+		dto = factory.getBean("user3", User.class);
+
+		System.out.println(dto.getName());
+		System.out.println(dto.getAddress());
+
+		System.out.println("------------------");
+
+		dto = factory.getBean("user4", User.class);
+
+		System.out.println(dto.getName());
+		System.out.println(dto.getAddress());
+
+	}
+
+}
