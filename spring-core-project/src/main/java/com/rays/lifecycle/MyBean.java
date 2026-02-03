8e6d59ff5829c6519ee340670dde8f8159ee2d55
Lucky <luckytomar1801@gmail.com>
2026-02-03 18:09:42 +0530

    done

---
+++ spring-core-project/src/main/java/com/rays/lifecycle/MyBean.java
@@ -0,0 +1,12 @@
+package com.rays.lifecycle;
+
+public class MyBean {
+
+	public void init() {
+		System.out.println("MyBean bean is created...");
+	}
+
+	public void cleanup() {
+		System.out.println("container close MyBean bean cleaned..");
+	}
+}
